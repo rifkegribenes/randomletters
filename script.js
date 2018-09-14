a3ebcd5f0610cb910114009ce90e4007c5f25974
rifkegribenes <rifkegribenes@gmail.com>
2018-09-13 22:30:44 -0700

    add spindoggy function

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const changeColor = () => {
-	document.getElementById('words').classList.toggle("green");
-}
+		const state = {
+			spinning: false,
+			bigText: false,
+			textGreen: false,
+			audioPlaying: false
+		};
 
-const embiggen = () => {
-	document.getElementById('words').classList.toggle("bigger");
-}
 
-const playSound = (val) => {
-		console.log(val);
-    var sound = document.createElement('audio');
-    sound.setAttribute('autoplay', 'autoplay');
-    sound.setAttribute('src', val);
-    sound.play();
-}
+		const spinDoggyFunction = () => {
+			console.log('spinDoggy');
+			document.getElementById('doggy-wrap').classList.toggle("invisible");
+			state.spinning = !state.spinning;
+			if (state.spinning) {
+				document.getElementById('spinDoggyText').innerHTML = "Stop Doggy";
+			} else {
+				document.getElementById('spinDoggyText').innerHTML = "Spin Doggy";
+			}
+		};
+
+		const changeColor = () => {
+			document.getElementById('words').classList.toggle("green");
+			state.textGreen = !state.textGreen;
+		};
+
+		const embiggen = () => {
+			document.getElementById('words').classList.toggle("bigger");
+			state.bigText = !state.bigText;
+		};
+
+		const playSound = (val) => {
+				console.log(val);
+		    var sound = document.createElement('audio');
+		    sound.setAttribute('autoplay', 'autoplay');
+		    sound.setAttribute('src', val);
+		    sound.play();
+		};
+
+
+window.onload = function(){
+
+		if (state.spinning) {
+			document.getElementById('spinDoggyText').innerHTML = "Stop Doggy";
+		} else {
+			document.getElementById('spinDoggyText').innerHTML = "Spin Doggy";
+		}
+
+	}
